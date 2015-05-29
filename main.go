bec8038860a902eafc5398ed3b6a81c111ccad59
slofuno <slofurno@gmail.com>
2015-05-29 10:12:12 -0400

    first

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+
+	ln, err := net.Listen("tcp", ":8080")
+
+	if err != nil {
+		// handle error
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			// handle error
+		}
+
+		go handleConnection(conn)
+	}
+
+}
+
+func handleConnection(conn net.Conn) {
+
+	buffer := make([]byte, 1024)
+
+	for {
+
+		length, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println(err)
+			return
+		} else {
+			fmt.Println(string(buffer[:length]))
+		}
+	}
+
+}
